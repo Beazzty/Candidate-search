ba23f5a09d21545b3b8bba9b161896ce0e930746
Quinn Brown <184797872+Beazzty@users.noreply.github.com>
2025-04-26 11:35:37 -0400

    removed develop folder

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,12 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    name: string | null;
+    company: string | null;
+    location:string | null;
+    email: string | null;
+    bio: string | null;
+}
